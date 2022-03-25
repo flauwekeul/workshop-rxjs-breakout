fe028f0afd3d12c2bae5ee1df7983bc19102bf40
Abbe Keultjes <flauwekeul@gmail.com>
2022-03-25 10:40:19 +0100

    replace giant updateEntities() with separate streams

--- finished/index.ts
+++ finished/index.ts
@@ -1,4 +1,15 @@
-import { animationFrameScheduler, combineLatest, interval, sampleTime, takeWhile, tap } from 'rxjs'
+import {
+  animationFrameScheduler,
+  combineLatest,
+  filter,
+  interval,
+  map,
+  merge,
+  partition,
+  sampleTime,
+  takeWhile,
+  tap,
+} from 'rxjs'
 import {
   BALL_INITIAL_DIRECTION,
   BALL_RADIUS,
@@ -55,47 +66,6 @@ const score$ = createScoreSubject(0)
 // FAR_LEFT_BOUNCE_DIRECTION and FAR_RIGHT_BOUNCE_DIRECTION based on this normalized value
 const paddleBounce = lerp(FAR_LEFT_BOUNCE_DIRECTION, FAR_RIGHT_BOUNCE_DIRECTION)
 
-const updateEntities = ({ paddle, ball, bricks, lives, score }: Entities): void => {
-  if (ball.speed === 0) {
-    canvas.classList.remove('hide-cursor')
-    const { x, y } = centerTopOfPaddle(paddle)
-    ball.x = x
-    ball.y = y
-    return
-  }
-
-  canvas.classList.add('hide-cursor')
-
-  if (hasBallPassedPaddle(ball.y, paddle)) {
-    lives$.next(--lives)
-    if (lives > 0) {
-      ball$.next(initialBall)
-    }
-    return
-  }
-
-  const brickCollision = getBrickCollision(ball, bricks)
-  if (brickCollision) {
-    const { brickIndex, hasCollidedVertically } = brickCollision
-    ball.direction = ball.direction * -1 + (hasCollidedVertically ? 0 : 180)
-    ball.speed *= BALL_SPEED_INCREASE
-    const bricksWithoutCollidedBrick = bricks.filter((_, i) => i !== brickIndex)
-    bricks$.next(bricksWithoutCollidedBrick)
-    score$.next(score + BRICK_SCORE)
-  } else if (hasBallTouchedPaddle(ball, paddle)) {
-    const normalizedPaddleImpactPosition = (ball.x - paddle.x) / PADDLE_WIDTH
-    ball.direction = paddleBounce(normalizedPaddleImpactPosition)
-  } else if (hasBallTouchedSide(ball, canvas.width)) {
-    ball.direction *= -1
-  } else if (hasBallTouchedTop(ball)) {
-    ball.direction = ball.direction * -1 + 180
-  }
-
-  const { x, y } = nextBallPosition(ball)
-  ball.x = x
-  ball.y = y
-}
-
 const render = ({ paddle, ball, bricks, lives, score }: Entities): void => {
   // clear previous renders
   canvasContext.clearRect(0, 0, canvas.width, canvas.height)
@@ -107,11 +77,103 @@ const render = ({ paddle, ball, bricks, lives, score }: Entities): void => {
   renderScore(canvasContext, score)
 }
 
-combineLatest({ tick: ticks$, paddle: paddle$, ball: ball$, bricks: bricks$, lives: lives$, score: score$ })
+const input$ = combineLatest({
+  tick: ticks$,
+  paddle: paddle$,
+  ball: ball$,
+  bricks: bricks$,
+  lives: lives$,
+  score: score$,
+}).pipe(
+  // make the stream emit only as fast as TICK_INTERVAL, this prevents mouse movements to make things move faster
+  sampleTime(TICK_INTERVAL, animationFrameScheduler)
+)
+
+const [whenBallOnPaddle$, whenBallNotOnPaddle$] = partition(input$, ({ ball }) => ball.speed === 0)
+
+const keepBallOnPaddle$ = whenBallOnPaddle$.pipe(
+  tap(({ paddle, ball }) => {
+    canvas.classList.remove('hide-cursor')
+    const { x, y } = centerTopOfPaddle(paddle)
+    ball.x = x
+    ball.y = y
+  })
+)
+
+const hideCursor$ = whenBallNotOnPaddle$.pipe(
+  tap(() => {
+    canvas.classList.add('hide-cursor')
+  })
+)
+
+const [whenBallPassesPaddle$, whenBallIsAbovePaddle$] = partition(whenBallNotOnPaddle$, ({ ball, paddle }) =>
+  hasBallPassedPaddle(ball.y, paddle)
+)
+
+const loseLifeAndResetBall$ = whenBallPassesPaddle$.pipe(
+  tap(({ lives }) => {
+    lives$.next(--lives)
+  }),
+  filter(({ lives }) => lives > 0),
+  tap(() => {
+    ball$.next(initialBall)
+  })
+)
+
+const whenBallCollidesWithBrick$ = whenBallIsAbovePaddle$.pipe(
+  map((entities) => ({ ...entities, brickCollision: getBrickCollision(entities.ball, entities.bricks) })),
+  filter(({ brickCollision }) => !!brickCollision),
+  tap(({ brickCollision, ball, bricks, score }) => {
+    const { brickIndex, hasCollidedVertically } = brickCollision
+    ball.direction = ball.direction * -1 + (hasCollidedVertically ? 0 : 180)
+    ball.speed *= BALL_SPEED_INCREASE
+    const bricksWithoutCollidedBrick = bricks.filter((_, i) => i !== brickIndex)
+    bricks$.next(bricksWithoutCollidedBrick)
+    score$.next(score + BRICK_SCORE)
+  })
+)
+
+const whenBallTouchesPaddle$ = whenBallIsAbovePaddle$.pipe(
+  filter(({ ball, paddle }) => hasBallTouchedPaddle(ball, paddle)),
+  tap(({ ball, paddle }) => {
+    const normalizedPaddleImpactPosition = (ball.x - paddle.x) / PADDLE_WIDTH
+    ball.direction = paddleBounce(normalizedPaddleImpactPosition)
+  })
+)
+
+const whenBallTouchesSide$ = whenBallIsAbovePaddle$.pipe(
+  filter(({ ball }) => hasBallTouchedSide(ball, canvas.width)),
+  tap(({ ball }) => {
+    ball.direction *= -1
+  })
+)
+
+const whenBallTouchesTop$ = whenBallIsAbovePaddle$.pipe(
+  filter(({ ball }) => hasBallTouchedTop(ball)),
+  tap(({ ball }) => {
+    ball.direction = ball.direction * -1 + 180
+  })
+)
+
+const updateBallPosition$ = whenBallIsAbovePaddle$.pipe(
+  tap(({ ball }) => {
+    const { x, y } = nextBallPosition(ball)
+    ball.x = x
+    ball.y = y
+  })
+)
+
+merge(
+  keepBallOnPaddle$,
+  hideCursor$,
+  loseLifeAndResetBall$,
+  whenBallCollidesWithBrick$,
+  whenBallTouchesPaddle$,
+  whenBallTouchesSide$,
+  whenBallTouchesTop$,
+  updateBallPosition$
+)
   .pipe(
-    // make the stream emit only as fast as TICK_INTERVAL, this prevents mouse movements to make things move faster
-    sampleTime(TICK_INTERVAL, animationFrameScheduler),
-    tap(updateEntities),
     tap(render),
     takeWhile(({ lives }) => lives > 0)
   )
