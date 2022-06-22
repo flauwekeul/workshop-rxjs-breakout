f4a735706bf07f9a6eea5bcf75520e65c732eb6a
Abbe Keultjes <flauwekeul@gmail.com>
2022-06-22 21:32:14 +0200

    implement step 2

--- src/ball.ts
+++ src/ball.ts
@@ -1,6 +1,10 @@
 import { Observable, of } from 'rxjs'
+import { BALL_COLOR } from '../shared/settings'
 import { Ball } from '../shared/types'
+import { drawCircle } from '../shared/utils'
 
-export const createBallStream = (initialBall: Ball, canvas: HTMLCanvasElement): Observable<Ball> => of()
+export const createBallStream = (initialBall: Ball, canvas: HTMLCanvasElement): Observable<Ball> => of(initialBall)
 
-export const renderBall = (canvasContext: CanvasRenderingContext2D, ball: Ball): void => {}
+export const renderBall = (canvasContext: CanvasRenderingContext2D, { x, y, radius }: Ball): void => {
+  drawCircle(canvasContext, { x, y, radius, color: BALL_COLOR })
+}
