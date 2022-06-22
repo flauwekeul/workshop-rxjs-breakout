482bc7ccd079c5e2875778aea8ad1d5f15f5c291
Abbe Keultjes <flauwekeul@gmail.com>
2022-06-22 21:09:43 +0200

    implement step 1

--- src/paddle.ts
+++ src/paddle.ts
@@ -1,6 +1,16 @@
-import { Observable, of } from 'rxjs'
+import { fromEvent, map, Observable, startWith } from 'rxjs'
+import { PADDLE_COLOR, PADDLE_HEIGHT, PADDLE_WIDTH } from '../shared/settings'
 import { Paddle } from '../shared/types'
+import { clamp, drawRectangle } from '../shared/utils'
 
-export const createPaddleStream = (paddle: Paddle, canvas: HTMLCanvasElement): Observable<Paddle> => of()
+export const createPaddleStream = (paddle: Paddle, canvas: HTMLCanvasElement): Observable<Paddle> => {
+  const keepOnScreen = clamp(0, canvas.width - PADDLE_WIDTH)
+  return fromEvent<MouseEvent>(canvas, 'mousemove').pipe(
+    map(({ clientX }) => ({ x: keepOnScreen(clientX - PADDLE_WIDTH / 2), y: paddle.y })),
+    startWith(paddle)
+  )
+}
 
-export const renderPaddle = (canvasContext: CanvasRenderingContext2D, paddle: Paddle): void => {}
+export const renderPaddle = (canvasContext: CanvasRenderingContext2D, { x, y }: Paddle): void => {
+  drawRectangle(canvasContext, { x, y, width: PADDLE_WIDTH, height: PADDLE_HEIGHT, fill: PADDLE_COLOR })
+}
