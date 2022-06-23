1631f77f8c1970331f639e6f8fced161925946e3
Abbe Keultjes <flauwekeul@gmail.com>
2022-06-23 20:44:47 +0200

    implement step 5

--- src/bricks.ts
+++ src/bricks.ts
@@ -1,6 +1,27 @@
 import { Observable, of } from 'rxjs'
+import { BRICKS_PER_ROW, BRICK_COLOR_MAP, BRICK_ROWS, BRICK_STROKE_COLOR } from '../shared/settings'
 import { Brick } from '../shared/types'
+import { brickBuilder, drawRectangle } from '../shared/utils'
 
-export const createBricksStream = (canvas: HTMLCanvasElement): Observable<Brick[]> => of()
+export const createBricksStream = (canvas: HTMLCanvasElement): Observable<Brick[]> => {
+  const createBrick = brickBuilder(canvas)
+  const rows = Array.from({ length: BRICK_ROWS })
+  const cols = Array.from({ length: BRICKS_PER_ROW })
+  const bricks = rows.flatMap((_, row) => cols.map((_, col) => createBrick(col, row)))
 
-export const renderBricks = (canvasContext: CanvasRenderingContext2D, bricks: Brick[]): void => {}
+  return of(bricks)
+}
+
+export const renderBricks = (canvasContext: CanvasRenderingContext2D, bricks: Brick[]): void => {
+  bricks.forEach(({ x, y, width, height, color }) => {
+    drawRectangle(canvasContext, {
+      x,
+      y,
+      width,
+      height,
+      fill: BRICK_COLOR_MAP[color],
+      strokeWidth: 3,
+      strokeColor: BRICK_STROKE_COLOR,
+    })
+  })
+}
