2ca8f02c3c4d0b2b05809966f5bc3cca6572c6b0
Abbe Keultjes <flauwekeul@gmail.com>
2022-06-23 21:15:11 +0200

    implement step 6

--- src/bricks.ts
+++ src/bricks.ts
@@ -1,15 +1,15 @@
-import { Observable, of } from 'rxjs'
+import { BehaviorSubject } from 'rxjs'
 import { BRICKS_PER_ROW, BRICK_COLOR_MAP, BRICK_ROWS, BRICK_STROKE_COLOR } from '../shared/settings'
 import { Brick } from '../shared/types'
 import { brickBuilder, drawRectangle } from '../shared/utils'
 
-export const createBricksStream = (canvas: HTMLCanvasElement): Observable<Brick[]> => {
+export const createBricksSubject = (canvas: HTMLCanvasElement): BehaviorSubject<Brick[]> => {
   const createBrick = brickBuilder(canvas)
   const rows = Array.from({ length: BRICK_ROWS })
   const cols = Array.from({ length: BRICKS_PER_ROW })
   const bricks = rows.flatMap((_, row) => cols.map((_, col) => createBrick(col, row)))
 
-  return of(bricks)
+  return new BehaviorSubject(bricks)
 }
 
 export const renderBricks = (canvasContext: CanvasRenderingContext2D, bricks: Brick[]): void => {
