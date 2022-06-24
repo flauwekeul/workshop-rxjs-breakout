ae1e6901fc7ed1908448f9a4a12aa5f1a84dcf9f
Abbe Keultjes <flauwekeul@gmail.com>
2022-06-24 10:20:37 +0200

    implement step 7

--- src/ball.ts
+++ src/ball.ts
@@ -1,14 +1,21 @@
-import { fromEvent, map, Observable, startWith, take } from 'rxjs'
+import { BehaviorSubject, filter, fromEvent, map } from 'rxjs'
 import { BALL_COLOR, BALL_INITIAL_SPEED } from '../shared/settings'
 import { Ball } from '../shared/types'
 import { drawCircle } from '../shared/utils'
 
-export const createBallStream = (initialBall: Ball, canvas: HTMLCanvasElement): Observable<Ball> =>
-  fromEvent<MouseEvent>(canvas, 'click').pipe(
-    take(1),
-    map(() => ({ ...initialBall, speed: BALL_INITIAL_SPEED })),
-    startWith(initialBall)
+export const createBallSubject = (initialBall: Ball, canvas: HTMLCanvasElement): BehaviorSubject<Ball> => {
+  const subject = new BehaviorSubject(initialBall)
+  const startMovingEvents$ = fromEvent<MouseEvent>(canvas, 'click').pipe(
+    // only "pass through" clicks if the current ball has no speed (i.e. sits on the paddle)
+    filter(() => subject.getValue().speed === 0),
+    // map the click event to a ball object with a speed > 0
+    map(() => ({ ...initialBall, speed: BALL_INITIAL_SPEED }))
   )
+
+  // make the subject observe the "click events that make the ball move"
+  startMovingEvents$.subscribe(subject)
+  return subject
+}
 
 export const renderBall = (canvasContext: CanvasRenderingContext2D, { x, y, radius }: Ball): void => {
   drawCircle(canvasContext, { x, y, radius, color: BALL_COLOR })
